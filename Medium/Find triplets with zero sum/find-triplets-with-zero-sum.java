4ef1179a492cd48cb34e5b8b893d1af62c6602f1
vanshikatiwary <107793386+vanshikatiwary@users.noreply.github.com>
2024-03-23 19:59:45 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find triplets with zero sum/find-triplets-with-zero-sum.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+import java.util.*;
+class Triplets{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			int n=sc.nextInt();
+			int[] a=new int[n];
+			for(int i=0;i<n;i++){
+				a[i]=sc.nextInt();
+			}
+			Solution g=new Solution();
+			if(g.findTriplets(a,n))
+				System.out.println("1");
+			else
+				System.out.println("0");
+			
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+
+class Solution
+{
+    // arr[]: input array
+    // n: size of the array
+    //Function to find triplets with zero sum.
+    public boolean findTriplets(int arr[] , int n)
+    {
+        Arrays.sort(arr);
+        for(int i=0;i<n;i++){
+            int start = i+1;
+            int end = n-1;
+            
+            while(start<end){
+                if(arr[i]+arr[start]+arr[end]==0){
+                    return true;
+                }
+                else if(arr[i]+arr[start]+arr[end]<0){
+                    start++;
+                }
+                else{
+                    end--;
+                }
+            }
+        }
+        return false;
+    }
+}
+
