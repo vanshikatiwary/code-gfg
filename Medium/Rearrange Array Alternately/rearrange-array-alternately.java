af9d12876dfd8435fb1de60a1a2457fe2fb38c3b
vanshikatiwary <107793386+vanshikatiwary@users.noreply.github.com>
2024-04-10 21:54:50 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Rearrange Array Alternately/rearrange-array-alternately.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args)throws IOException
+	{
+
+// 		Scanner in = new Scanner(System.in);
+        
+// 		int t = in.nextInt();
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+         //testcases
+        int t = Integer.parseInt(read.readLine());
+		
+		while(t-- >0)
+		{
+		    //size of array
+		    int n = Integer.parseInt(read.readLine());
+		    long[] arr= new long[n];
+		    
+		    String str[] = read.readLine().trim().split(" ");
+		    
+		    //adding elements to the array
+		    for(int i=0;i<n;i++)
+		        arr[i] = Long.parseLong(str[i]);
+		    
+		    // StringBuffer sb = new StringBuffer();
+		    
+		    Solution ob = new Solution();
+		    
+		    //calling rearrange() function
+		    ob.rearrange(arr, n);
+		    StringBuffer sb = new StringBuffer();
+		    
+		    //appending and printing the elements
+		    for(int i =0; i < n; i++)
+		     sb.append(arr[i] + " ");
+		    System.out.println(sb);
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+class Solution{
+    
+    // temp: input array
+    // n: size of array
+    //Function to rearrange  the array elements alternately.
+    public static void rearrange(long arr[], int n){
+        
+        // Your code here
+          long ari[]=new long[n];
+        for(int i = 0;i<n;i++){
+            ari[i]=arr[i];
+            
+            
+        }
+        Arrays.sort(ari);
+int startingindex = 0;
+int endingindex = n-1;
+for(int i =0;i<n;i++){
+arr[i]= ari[endingindex];
+endingindex--;
+i++;
+}
+for(int i =1;i<n;i++){
+    arr[i]= ari[startingindex];
+    startingindex++;
+    i++;
+    }
+}
+}
+
