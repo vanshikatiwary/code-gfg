7c274bab917992192452020642ac8ab4aa4a963f
vanshikatiwary <107793386+vanshikatiwary@users.noreply.github.com>
2024-05-30 01:06:55 +0530

    Update day 77.java

--- 100 days of code/day 77.java
+++ 100 days of code/day 77.java
@@ -1,14 +1,17 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-public class Main
-{
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+class Solution {
+    public static int minimumCost(int n, int w, int[] cost) {
+        // code here
+        int dp[] = new int[w + 1];
+        for(int i = 0; i <= w; i++)
+            dp[i] = Integer.MAX_VALUE;
+        
+        dp[0] = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j <= w; j++)
+                dp[j] = Math.min(dp[j], cost[i] + dp[j - i - 1]); 
+    
+        return dp[w];
+    }
 }
+  
