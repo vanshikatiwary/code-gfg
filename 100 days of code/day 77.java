6b9ffcf0b1b771ac747dd9620592698523950f30
vanshikatiwary <107793386+vanshikatiwary@users.noreply.github.com>
2024-05-30 01:28:04 +0530

    Add files via upload

--- 100 days of code/day 77.java
+++ 100 days of code/day 77.java
@@ -1,17 +1,16 @@
-
 class Solution {
-    public static int minimumCost(int n, int w, int[] cost) {
+    public static int findWinner(int n, int x, int y) {
         // code here
-        int dp[] = new int[w + 1];
-        for(int i = 0; i <= w; i++)
-            dp[i] = Integer.MAX_VALUE;
+        int[] dp = new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
         
-        dp[0] = 0;
-        for (int i = 0; i < n; i++)
-            for (int j = i + 1; j <= w; j++)
-                dp[j] = Math.min(dp[j], cost[i] + dp[j - i - 1]); 
-    
-        return dp[w];
+        for(int i =2;i<=n;i++){
+            //geek can reach n in x, y or 1 step:
+            if( (i>=y && dp[i-y]==0) || (i>=x && dp[i-x]==0 ) || dp[i-1]==0) dp[i]=1;
+        }
+        
+        return dp[n];
     }
 }
-  
+   
