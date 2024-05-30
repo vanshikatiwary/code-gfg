683d1dae574e7d54567887ea1bc88e1fbb1988b1
vanshikatiwary <107793386+vanshikatiwary@users.noreply.github.com>
2024-05-30 17:00:59 +0530

    Update day 71.java

--- 100 days of code/day 71.java
+++ 100 days of code/day 71.java
@@ -1,31 +1,29 @@
 class Solution {
-    public static int noOfGasStationsBetween(int[] arr, double dist) {
-        int cnt = 0, n = arr.length;
-    
-        for (int i = 0; i < n - 1; i++) {
-          double stationsInBetween = (arr[i + 1] - arr[i]) / dist;
-          cnt += stationsInBetween;
+     static int mod = (int) 1e9+7;
+    public static int countPartitions(int n, int d, int[] arr) {
+        // code here
+        int sum = 0;
+        for(int num : arr) sum += num;
+        if( (sum-d) % 2 == 1) return 0;
+        int half = (sum-d)/2;
+        if(half < 0) return 0;
+        int[][] dp = new int[n][half+1];
+        for(int[] temp : dp) {
+            Arrays.fill(temp, -1);
         }
-    
-        return cnt;
-    }
-
-    public static double findSmallestMaxDist(int[] arr, int k) {
-        int n = arr.length;
-        double maxi = Integer.MIN_VALUE;
-        for (int i = 0; i < n - 1; i++) maxi =
-          Math.max(maxi, (double) (arr[i + 1] - arr[i]));
-    
-        double low = 0, high = maxi, diff = 1e-6; // 10^-6
-        while(high - low > diff) {
-            double mid = low + (high - low) / 2;
-            if(noOfGasStationsBetween(arr, mid) <= k)
-                high=mid;
-            else    
-                low=mid;
-        }
+        return helper(half, 0, n, arr, 0, dp)%mod;
         
-        return high;
     }
-};
-
+    
+    private static int helper(int sum, int temp, int n, int[] arr, int idx, int[][] dp) {
+        if(idx == n || temp > sum) {
+            if(sum == temp) return 1;
+            return 0;
+        } 
+        if(dp[idx][temp] != -1) return dp[idx][temp]%mod;
+        int ans = 0;
+        ans += helper(sum, temp+arr[idx], n, arr, idx+1, dp);
+        ans += helper(sum, temp, n, arr, idx+1,dp);
+        return dp[idx][temp] = ans%mod;
+    }
+}
