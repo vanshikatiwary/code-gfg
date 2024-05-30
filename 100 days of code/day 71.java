f534893a35f6a233e679eb29524cd3f813421a8b
vanshikatiwary <107793386+vanshikatiwary@users.noreply.github.com>
2024-05-30 17:02:45 +0530

    Update day 71.java

--- 100 days of code/day 71.java
+++ 100 days of code/day 71.java
@@ -1,29 +1,20 @@
 class Solution {
-     static int mod = (int) 1e9+7;
-    public static int countPartitions(int n, int d, int[] arr) {
+    public int kPalindrome(String str, int n, int k) {
         // code here
-        int sum = 0;
-        for(int num : arr) sum += num;
-        if( (sum-d) % 2 == 1) return 0;
-        int half = (sum-d)/2;
-        if(half < 0) return 0;
-        int[][] dp = new int[n][half+1];
-        for(int[] temp : dp) {
-            Arrays.fill(temp, -1);
+         int dp[][] = new int[n][n];
+        for(int i = 0; i < n; i++)
+            dp[i][i] = 0;
+        for(int len = 2; len <= n; len++) {
+            for(int i = 0; i < n - len + 1; i++) {
+                int j = i + len - 1;
+                if(str.charAt(i) == str.charAt(j))
+                    dp[i][j] = dp[i + 1][j - 1];
+                else
+                    dp[i][j] = 1 + Math.min(dp[i + 1][j], dp[i][j - 1]);
+            }
         }
-        return helper(half, 0, n, arr, 0, dp)%mod;
-        
-    }
-    
-    private static int helper(int sum, int temp, int n, int[] arr, int idx, int[][] dp) {
-        if(idx == n || temp > sum) {
-            if(sum == temp) return 1;
-            return 0;
-        } 
-        if(dp[idx][temp] != -1) return dp[idx][temp]%mod;
-        int ans = 0;
-        ans += helper(sum, temp+arr[idx], n, arr, idx+1, dp);
-        ans += helper(sum, temp, n, arr, idx+1,dp);
-        return dp[idx][temp] = ans%mod;
+        return dp[0][n - 1] <= k ? 1 : 0;
     }
 }
+
+  
