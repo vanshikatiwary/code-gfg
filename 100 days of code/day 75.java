82bc0c43523491da1a0ed23546c117dc0802ac9e
vanshikatiwary <107793386+vanshikatiwary@users.noreply.github.com>
2024-05-30 17:07:04 +0530

    Update day 75.java

--- 100 days of code/day 75.java
+++ 100 days of code/day 75.java
@@ -1,35 +1,18 @@
 class Solution {
-    
-    public int findMinCost(String x, String y, int costX, int costY) {
-        
-         int a = x.length();
-         int b = y.length();
-        
-        int arr[] = new int[b+1], prev[] = new int[b+1];
-        
-        for(int i=1; i<=a; i++){
-            for(int j=1; j<=b; j++){
-                
-                if(x.charAt(i-1) == y.charAt(j-1))
-                    {
-                        
-                    arr[j] = prev[j-1] + 1;
-                }else
-                {
-                    
-                    arr[j] = Math.max( arr[j-1], prev[j] );
+    public static int longestSubseq(int n, int[] a) {
+        // code here
+         int dp[]= new int[n];
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(Math.abs(a[i]-a[j])==1){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
                 }
             }
-            
-            prev = arr;
-            arr = new int[b+1];
         }
-        
-        int ans = prev[b];
-        
-        int p = a-ans;
-        int q = b-ans;
-        
-        return p*costX + q*costY;
+        int ans=0;
+        for(int u=0;u<n;u++){
+            if(dp[u]>ans)ans=dp[u];
+        }
+        return ans+1;
     }
 }
