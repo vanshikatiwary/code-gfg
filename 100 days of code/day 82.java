b139cad072f238d2b62186dff87337578f16a30a
vanshikatiwary <107793386+vanshikatiwary@users.noreply.github.com>
2024-06-04 23:43:14 +0530

    Add files via upload

---
+++ 100 days of code/day 82.java
@@ -0,0 +1,19 @@
+class Solution {
+    String binaryNextNumber(String s) {
+        // code here.
+         String ans = "";
+        for(int i = s.length() - 1; i >= 0; i--) {
+            if(s.charAt(i) == '1') {
+                ans = "0" + ans;;
+                if(i == 0) {
+                    return "1" + ans;
+                }
+            } else {
+                ans = s.substring(0, i) + "1" + ans;
+                break;
+            }
+        }
+        ans = ans.replaceAll("^[0]*", "");
+        return ans;
+    }
+}
