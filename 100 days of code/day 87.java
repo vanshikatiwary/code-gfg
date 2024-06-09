8ea6044a6c2af97e8d79bd7bdfcea5abf1938122
vanshikatiwary <107793386+vanshikatiwary@users.noreply.github.com>
2024-06-09 23:33:43 +0530

    Add files via upload

---
+++ 100 days of code/day 87.java
@@ -0,0 +1,23 @@
+class Solution {
+    public static void zigZag(int n, int[] arr) {
+        // code here
+        for (int i = 0; i < n - 1; i++) {
+            if (i % 2 == 0) {
+                if (arr[i] > arr[i + 1]) {
+                  
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            } else {
+                if (arr[i] < arr[i + 1]) {
+                  
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
