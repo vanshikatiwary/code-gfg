e9256688f5a23f0de5e155985e0261ddecc422c3
vanshikatiwary <107793386+vanshikatiwary@users.noreply.github.com>
2024-06-10 23:37:14 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Nuts and Bolts Problem/nuts-and-bolts-problem.java
@@ -0,0 +1,108 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            char[] nuts = new char[n], bolts = new char[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                nuts[i] = (inputLine[i].charAt(0));
+            }
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                bolts[i] = (inputLine[i].charAt(0));
+            }
+
+            new Solution().matchPairs(n, nuts, bolts);
+            for (int i = 0; i < n; i++) {
+                System.out.print(nuts[i] + " ");
+            }
+            System.out.println();
+            for (int i = 0; i < n; i++) {
+                System.out.print(bolts[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    void matchPairs(int n, char nuts[], char bolts[]) {
+         // code here
+         HashSet<Character> hs = new HashSet<>();
+        for(int i = 0; i < n; i++)
+        {
+            hs.add(nuts[i]);
+        }
+        
+        int j = 0;
+        if(hs.contains('!'))
+        {
+            nuts[j] = '!';
+            bolts[j] = '!';
+            j++;
+        }
+        if(hs.contains('#'))
+        {
+            nuts[j] = '#';
+            bolts[j] = '#';
+            j++;
+        }
+        if(hs.contains('$'))
+        {
+            nuts[j] = '$';
+            bolts[j] = '$';
+            j++;
+        }
+        if(hs.contains('%'))
+        {
+            nuts[j] = '%';
+            bolts[j] = '%';
+            j++;
+        }
+        if(hs.contains('&'))
+        {
+            nuts[j] = '&';
+            bolts[j] = '&';
+            j++;
+        }
+        if(hs.contains('*'))
+        {
+            nuts[j] = '*';
+            bolts[j] = '*';
+            j++;
+        }
+        if(hs.contains('?'))
+        {
+            nuts[j] = '?';
+            bolts[j] = '?';
+            j++;
+        }
+        if(hs.contains('@'))
+        {
+            nuts[j] = '@';
+            bolts[j] = '@';
+            j++;
+        }
+        if(hs.contains('^'))
+        {
+            nuts[j] = '^';
+            bolts[j] = '^';
+            j++;
+        }
+    }
+}
