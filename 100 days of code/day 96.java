3f68b339711925badcf4b2e17b4aca0f7543ab0b
vanshikatiwary <107793386+vanshikatiwary@users.noreply.github.com>
2024-06-18 23:01:20 +0530

    Add files via upload

---
+++ 100 days of code/day 96.java
@@ -0,0 +1,15 @@
+class Solution {
+    int rectanglesInCircle(int r) {
+        // code here
+               int ans=0;
+        int x=r*r;
+        for(int i=1;i<=2*r;i++){
+            for(int j=1;j<=2*r;j++){
+                if((i*i)+(j*j)<=4*x){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
