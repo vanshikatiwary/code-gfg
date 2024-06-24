6397a8826db39e89cfed3d3b7100fbbd1e3bde0d
vanshikatiwary <107793386+vanshikatiwary@users.noreply.github.com>
2024-06-24 23:50:01 +0530

    Update day 10.java

--- 100 days of code/day 10.java
+++ 100 days of code/day 10.java
@@ -1,11 +1,4 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 class Solution
 {
     //Function to find the smallest positive number missing from the array.
