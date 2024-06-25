3de75b5ef2aa48e0ec26c5933f2b0de2514059cc
vanshikatiwary <107793386+vanshikatiwary@users.noreply.github.com>
2024-06-25 22:57:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Integral Points Inside Triangle/integral-points-inside-triangle.java
+++ Difficulty: Medium/Integral Points Inside Triangle/integral-points-inside-triangle.java
@@ -32,31 +32,27 @@ class GFG {
 // User function Template for Java
 
 class Solution {
-    long InternalCount(long p[], long q[], long r[]) {
-        // code here
-        Point P = new Point(p[0], p[1]);
-        Point Q = new Point(q[0], q[1]);
-        Point R = new Point(r[0], r[1]);
-        long boundary = get1(P, Q) + get1(Q, R) + get1(R, P) + 3;
-        long area = Math.abs(P.x * (Q.y - R.y) + Q.x * (R.y - P.y) + R.x * (P.y - Q.y));
-        long internalPoints = (area - boundary + 2) / 2;
-        return internalPoints;
-    }
-   public long get1(Point p, Point q) {
-        if (p.x == q.x) return Math.abs(p.y - q.y) - 1;
-        if (p.y == q.y) return Math.abs(p.x - q.x) - 1;
-        return gcd(Math.abs(p.x - q.x), Math.abs(p.y - q.y)) - 1;
-    }
-    public long gcd(long a, long b) {
-        if (b == 0) return a;
+    
+    public static long gcd(long a, long b) {
+        if (b == 0)
+            return a;
         return gcd(b, a % b);
     }
-    class Point {
-        long x, y;
-        Point(long x, long y) {
-            this.x = x;
-            this.y = y;
+    
+    long InternalCount(long p[], long q[], long r[]) {
+        // code here
+        
+        //area
+        long area = Math.abs((p[0] * (q[1] - r[1]) + q[0] * (r[1] - p[1]) + r[0] * (p[1] - q[1])) / 2);
+        
+        //boundary
+        long B1 = gcd(Math.abs(p[0] - q[0]), Math.abs(p[1] - q[1])) + 1;
+        long B2 = gcd(Math.abs(q[0] - r[0]), Math.abs(q[1] - r[1])) + 1;
+        long B3 = gcd(Math.abs(r[0] - p[0]), Math.abs(r[1] - p[1])) + 1;
+        
+        long B = (B1 + B2 + B3 - 3) / 2;
+        long I = area - B + 1;
+        
+        return I;
     }
-}
-}
-   
+};
